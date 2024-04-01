b854cf0d9b3b14bb99b06bdc10a709735b6e7b42
Santiago Sierra <santiagosierra2003@gmail.com>
2024-04-01 18:39:55 -0500

    Agregué un contador

--- ejemplo.js
+++ ejemplo.js
@@ -1,3 +1,7 @@
+if (!localStorage.getItem("contador")){
+    localStorage.setItem("contador", 0);
+}
+
 function hola(){
     const aux = document.querySelector("h2");
 
@@ -8,7 +12,25 @@ function hola(){
     }
 }
 
+function contar(){
+    let contador = localStorage.getItem("contador");
+    contador++;
+    document.getElementById("contador").innerHTML = contador;
+    localStorage.setItem("contador", contador);
+}
+
+function borrar(){
+    let contador = 0;
+    document.getElementById("contador").innerHTML = contador;
+    localStorage.setItem("contador", contador);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("contador").innerHTML = localStorage.getItem("contador");
+
+    document.querySelector("#contar").onclick = contar;
+    document.querySelector("#reset").onclick = borrar;
 
     //Cambiar elemento
     document.querySelector("#click").onclick = hola;
